935cefd84df4fb7c8a1e93a1fb5f0312953e9b15
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2016-02-03 12:18:14 +0100

    Remove unused controller test

--- test/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Exremit.PageControllerTest do
-  use Exremit.ConnCase
-
-  test "GET /" do
-    conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
