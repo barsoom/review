6baa8954b163c0be8577044241d5522f052cd942
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2016-02-03 14:17:14 +0100

    Try to unbreak CI

--- script/ci/tests.sh
+++ script/ci/tests.sh
@@ -5,6 +5,6 @@ export PATH="$HOME/dependencies/erlang/bin:$HOME/dependencies/elixir/bin:$PATH"
 
 # Start headless browser server used by javascript-enabled acceptance tests
 phantomjs -w > /dev/null &
-sleep 0.5
+sleep 2
 
 mix test
