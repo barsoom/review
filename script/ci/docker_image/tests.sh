e5170b9bc8b0d5bf221e350e8f5cea22e7fd6ea4
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2016-02-08 01:08:22 +0100

    Cleanup

--- script/ci/docker_image/tests.sh
+++ script/ci/docker_image/tests.sh
@@ -2,13 +2,11 @@
 set -e
 
 export MIX_ENV=test
-
-cd /app
-
 export PATH="/app/node_modules/.bin:$PATH"
 
 # Start headless browser server used by javascript-enabled acceptance tests
 phantomjs -w > /dev/null &
 sleep 1
 
+cd /app
 mix test
