239a8ea549bc65aed899d24313b4b25596e74aaf
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2016-02-08 08:09:46 +0100

    Cleanup

--- script/ci/docker_image/bootstrap.sh
+++ script/ci/docker_image/bootstrap.sh
@@ -2,6 +2,7 @@
 set -e
 
 export MIX_ENV=test
+export PATH="/app/node_modules/.bin:$PATH"
 
 # Install package tools
 if [ ! -e $HOME/.mix/rebar ]; then
@@ -9,13 +10,9 @@ if [ ! -e $HOME/.mix/rebar ]; then
   yes Y | LC_ALL=en_GB.UTF-8 mix local.rebar
 fi
 
-
 cd /app
-
 mix do deps.get, deps.compile, compile
 npm install
-
-export PATH="/app/node_modules/.bin:$PATH"
 
 cd web/elm
 elm package install -y
--- script/ci/prepare.sh
+++ script/ci/prepare.sh
@@ -18,23 +18,6 @@ docker run -v ~/exremit:/app -v ~/.mix:/root/.mix exremit /app/script/ci/docker_
 
 exit 0
 
-PHANTOMJS_VERSION=phantomjs-2.1.1-linux-x86_64
-PHANTOMJS_PATH=$HOME/$PHANTOMJS_VERSION
-PHANTOMJS_SHA="86dd9a4bf4aee45f1a84c9f61cf1947c1d6dce9b9e8d2a907105da7852460d2f"
-
-if [ ! -e $PHANTOMJS_PATH ]; then
-  cd ~
-  wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOMJS_VERSION.tar.bz2
-  echo "$PHANTOMJS_SHA  phantomjs-2.1.1-linux-x86_64.tar.bz2" | sha256sum -c -
-  tar xfj $PHANTOMJS_VERSION.tar.bz2
-  ln -sf $PHANTOMJS_PATH $HOME/dependencies/phantomjs
-fi
-
-# Fetch and compile dependencies and application code (and include testing tools)
-export MIX_ENV="test"
-cd $HOME/$CIRCLE_PROJECT_REPONAME
-mix do deps.get, deps.compile, compile
-
 if [ ! -e _build/.node-fixed ]; then
   # TODO: try other options: https://github.com/phoenixframework/phoenix/issues/1410
   npm install --save-dev babel-preset-es2015 && touch _build/.node-fixed
