afd3d57fd1dd34de9de2b37bfe25eccbadb2b042
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2016-02-08 21:50:07 +0100

    Fix deps caching
    deps.compile is not needed

--- script/ci/prepare.sh
+++ script/ci/prepare.sh
@@ -67,6 +67,6 @@ fi
 # Fetch and compile dependencies and application code (and include testing tools)
 export MIX_ENV="test"
 cd $HOME/$CIRCLE_PROJECT_REPONAME
-mix do deps.get, deps.compile, compile
+mix do deps.get, compile
 
 script/install_dependencies
