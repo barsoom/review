3540b86525d9dd2560ecc87512b1a0c180f71c93
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2016-02-18 17:29:04 +0100

    Cleanup

--- test/acceptance/settings_test.exs
+++ test/acceptance/settings_test.exs
@@ -27,6 +27,4 @@ defmodule Exremit.SettingsTest do
   defp navigate_to_settings_page, do: navigate_to "/settings?auth_key=secret"
 
   defp fill_in(field, with: value), do: find_element(:name, field) |> fill_field(value)
-
-  defp click_save, do: find_element(:type, "submit") |> click
 end
