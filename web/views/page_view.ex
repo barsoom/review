a97cb1645b8a8ef96587092358bc705fea6c89ef
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2016-06-09 13:41:12 +0200

    Fix render_elm serialization of single quotes
    It's the only character that can't be in the content as it's the terminator.

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -2,7 +2,7 @@ defmodule Exremit.PageView do
   use Exremit.Web, :view
 
   def render_elm(app_name, options \\ []) do
-    options_json = options |> Enum.into(%{}) |> Poison.encode!
+    options_json = options |> Enum.into(%{}) |> Poison.encode! |> String.replace("'", "\\u0027")
 
     """
     <div class="js-elm-app" data-app-name="#{app_name}" data-options='#{options_json}'></div>
