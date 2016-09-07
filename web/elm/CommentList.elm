44d035c2c42c1fd4676f14b7bf07e1b5c4df2594
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2016-09-07 19:14:43 +0200

    Add comment settings options

--- web/elm/CommentList.elm
+++ web/elm/CommentList.elm
@@ -5,9 +5,15 @@ import Html.Attributes exposing (..)
 import VirtualDom exposing (Node)
 import Types exposing (..)
 
+-- TODO:
+-- filter things based on settings
+
 view : Model -> Html a
 view model =
-  ul [ class "comments-list" ] (List.map (renderComment model) model.comments)
+  div [] [
+    renderCommentSettings
+  , renderCommentList(model)
+  ]
 
 renderComment : Model -> Comment -> Node a
 renderComment model comment =
@@ -23,6 +29,25 @@ renderComment model comment =
       ]
     ]
   ]
+
+renderCommentSettings : Node a
+renderCommentSettings =
+  div [ class "comment-settings" ] [
+    renderOption "Comments I wrote"
+  , renderOption "Comments on others"
+  , renderOption "Resolved comments"
+  ]
+
+renderOption : String -> Node a
+renderOption name =
+  label [] [
+    input [ type' "checkbox" ] []
+  ,  text name
+  ]
+
+renderCommentList : Model -> Node a
+renderCommentList model =
+  ul [ class "comments-list" ] (List.map (renderComment model) model.comments)
 
 commentId : Comment -> String
 commentId comment =
