876176ddb110dbd1a039d165b3f9d83f5cd51337
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2016-10-09 10:53:42 +0200

    Test api auth

--- test/controllers/api_controller_test.exs
+++ test/controllers/api_controller_test.exs
@@ -2,7 +2,14 @@ defmodule Review.ApiControllerTest do
   use Review.ConnCase
   import Review.Factory
 
-  test "unreviewed_commit_stats works" do
+  test "unreviewed_commit_stats denies users with an invalid secret key" do
+    conn = build_conn
+      |> get("/api/v1/unreviewed_commit_stats?secret=invalid_secret")
+
+    assert response(conn, 403) =~ "Denied (probably an invalid key?)"
+  end
+
+  test "unreviewed_commit_stats returns data when there are no commits" do
     conn = build_conn
       |> get("/api/v1/unreviewed_commit_stats?secret=api_secret")
 
@@ -10,7 +17,9 @@ defmodule Review.ApiControllerTest do
       "count" => 0,
       "oldest_age_in_seconds" => nil
     }
+  end
 
+  test "unreviewed_commit_stats returns data when there is commits" do
     insert(:commit)
 
     conn = build_conn
