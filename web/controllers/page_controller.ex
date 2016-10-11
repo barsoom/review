d398e2e47923d9d7b918ff579957e236b75dbb21
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2016-10-11 17:41:04 +0200

    Add a boom route

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,6 +1,10 @@
 defmodule Review.PageController do
   use Review.Web, :controller
 
+  def index(_conn, %{ "page" => "boom" }) do
+    raise "this is an error made to test the error reporter"
+  end
+
   def index(conn, _params) do
     render conn, "index.html", commits_data: commits_data, comments_data: comments_data
   end
