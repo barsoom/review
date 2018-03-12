56e194f77759410b2c48804576f2b3ceb2f5e41d
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2018-03-12 16:58:58 +0100

    Correct postgres in CI and maybe fix install step

--- script/ci/prepare.sh
+++ script/ci/prepare.sh
@@ -6,7 +6,7 @@ if [ ! -d $INSTALL_PATH/sysconfcpus/bin ]; then
   git clone https://github.com/obmarg/libsysconfcpus.git
   cd libsysconfcpus
   ./configure --prefix=$INSTALL_PATH/sysconfcpus
-  make && make install
+  make && sudo make install
   cd ..
 fi
 
