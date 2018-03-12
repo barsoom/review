8a652b88085a3efd809cc6d263b0eeb5ae4723e6
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2018-03-12 16:59:42 +0100

    Fix path

--- script/ci/prepare.sh
+++ script/ci/prepare.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+export INSTALL_PATH="$HOME/dependencies"
+
 if [ ! -d $INSTALL_PATH/sysconfcpus/bin ]; then
   git clone https://github.com/obmarg/libsysconfcpus.git
   cd libsysconfcpus
   ./configure --prefix=$INSTALL_PATH/sysconfcpus
-  make && sudo make install
+  make && make install
   cd ..
 fi
 
