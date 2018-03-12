987342aa6d2ad5129f774b8a53c0d1ff01c27d98
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2018-03-12 17:34:01 +0100

    Try to fix deploy step
    It failed without error

--- script/ci/deploy.sh
+++ script/ci/deploy.sh
@@ -11,10 +11,10 @@ function _main {
 function _deploy_to_heroku {
   set +e
   git remote add heroku https://git.heroku.com/$app_name.git 2> /dev/null
-  set -e
 
   # Workaround for https://github.com/travis-ci/dpl/issues/127#issuecomment-42397378
   git fetch --unshallow 2> /dev/null
+  set -e
 
   git push heroku master
 }
