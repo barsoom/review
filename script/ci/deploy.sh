20337a04102d74708b36b7bb08a9b2f80c26360e
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2018-03-12 17:29:50 +0100

    Add deploy step

---
+++ script/ci/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+app_name=$1
+
+function _main {
+  _deploy_to_heroku
+}
+
+function _deploy_to_heroku {
+  set +e
+  git remote add heroku https://git.heroku.com/$app_name.git 2> /dev/null
+  set -e
+
+  # Workaround for https://github.com/travis-ci/dpl/issues/127#issuecomment-42397378
+  git fetch --unshallow 2> /dev/null
+
+  git push heroku master
+}
+
+_main
