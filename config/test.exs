29083ddfcfb7202c23d6b7929653dad05fb7c97a
Tomas Skogberg <tomas.skogberg@gmail.com>
2018-03-21 14:43:08 +0100

    Add devbox support

--- config/test.exs
+++ config/test.exs
@@ -10,13 +10,24 @@ config :review, Review.Endpoint,
 config :logger, level: :warn
 
 # Configure your database
-config :review, Review.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("POSTGRES_USER") || System.get_env("USER"),
-  password: "",
-  database: "review_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+if System.get_env("DEVBOX") do
+  config :review, Review.Repo,
+    adapter: Ecto.Adapters.Postgres,
+    username: "postgres",
+    password: "dev",
+    database: "review_test",
+    hostname: "localhost",
+    port: System.cmd("service_port", ["postgres"]) |> elem(0) |> String.trim,
+    pool: Ecto.Adapters.SQL.Sandbox
+else
+  config :review, Review.Repo,
+    adapter: Ecto.Adapters.Postgres,
+    username: System.get_env("POSTGRES_USER") || System.get_env("USER"),
+    password: "",
+    database: "review_test",
+    hostname: "localhost",
+    pool: Ecto.Adapters.SQL.Sandbox
+end
 
 config :review,
   auth_key: "secret",
