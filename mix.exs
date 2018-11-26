1366b3e8350766c9d13c452a722b8efe64195d0e
Tomas Skogberg <tomas.skogberg@gmail.com>
2018-11-26 15:41:55 +0100

    Update elixir/erlang (1.7.4/21.1) versions

--- mix.exs
+++ mix.exs
@@ -2,44 +2,58 @@ defmodule Review.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :review,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     elixirc_paths: elixirc_paths(Mix.env),
-     compilers: [:phoenix] ++ Mix.compilers,
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     aliases: aliases,
-     deps: deps]
+    [
+      app: :review,
+      version: "0.0.1",
+      elixir: "~> 1.0",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix] ++ Mix.compilers(),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases,
+      deps: deps
+    ]
   end
 
   # Configuration for the OTP application.
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {Review, []},
-     applications: [:phoenix, :phoenix_html, :cowboy, :logger,
-                    :phoenix_ecto, :postgrex, :calendar, :honeybadger]]
+    [
+      mod: {Review, []},
+      applications: [
+        :phoenix,
+        :phoenix_html,
+        :cowboy,
+        :logger,
+        :phoenix_ecto,
+        :postgrex,
+        :calendar,
+        :honeybadger
+      ]
+    ]
   end
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
-  defp elixirc_paths(_),     do: ["lib", "web"]
+  defp elixirc_paths(_), do: ["lib", "web"]
 
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, ">= 0.0.0"},
-     {:phoenix_html, ">= 0.0.0"},
-     {:phoenix_live_reload, ">= 0.0.0", only: :dev},
-     {:phoenix_ecto, ">= 0.0.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:calendar, "~> 0.14.2"},
-     {:honeybadger, "~> 0.1"},
-     {:hound, ">= 0.0.0", only: :test},
-     {:ex_machina, ">= 0.0.0", only: :test},
-     {:cowboy, ">= 0.0.0"}]
+    [
+      {:phoenix, ">= 0.0.0"},
+      {:phoenix_html, ">= 0.0.0"},
+      {:phoenix_live_reload, ">= 0.0.0", only: :dev},
+      {:phoenix_ecto, ">= 0.0.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:calendar, "~> 0.14.2"},
+      {:honeybadger, "~> 0.1"},
+      {:hound, ">= 0.0.0", only: :test},
+      {:ex_machina, ">= 0.0.0", only: :test},
+      {:cowboy, ">= 0.0.0"}
+    ]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
@@ -52,7 +66,7 @@ defmodule Review.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"],
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
