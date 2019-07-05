f99eaf1ceeecd6bbf56af69932038a0d97ec38c0
Tomas Skogberg and Peter Wall <all+tomas+peter@barsoom.se>
2019-07-05 16:48:59 +0200

    Add unique_constraint for email and username
    To prevent duplicate authors being created, see https://trello.com/c/XewtDoCs

--- test/lib/repo_test.exs
+++ test/lib/repo_test.exs
@@ -2,20 +2,23 @@ defmodule Review.RepoTest do
   use Review.ModelCase
 
   test "insert_or_update_author: can persist and find an author by email or username" do
-    author = Repo.insert_or_update_author(%{ email: "foo@example.com", username: "foo", name: "Foo" })
+    author =
+      Repo.insert_or_update_author(%{email: "foo@example.com", username: "foo", name: "Foo"})
 
     assert author.id > 0
     assert author.name == "Foo"
     assert author.username == "foo"
     assert author.email == "foo@example.com"
 
-    author2 = Repo.insert_or_update_author(%{ email: "foo@example.com", username: "foo", name: "Foo" })
+    author2 =
+      Repo.insert_or_update_author(%{email: "foo@example.com", username: "foo", name: "Foo"})
+
     assert author.id == author2.id
 
-    author2 = Repo.insert_or_update_author(%{ email: "foo@example.com" })
+    author2 = Repo.insert_or_update_author(%{email: "foo@example.com"})
     assert author.id == author2.id
 
-    author2 = Repo.insert_or_update_author(%{ username: "foo" })
+    author2 = Repo.insert_or_update_author(%{username: "foo"})
     assert author.id == author2.id
   end
 
@@ -23,10 +26,10 @@ defmodule Review.RepoTest do
   # about, i.e. a commit created before you started using this app.
   test "insert_or_update_author: allows duplicates when it can't be avoided" do
     # Github comment hook only gives us username
-    author1 = Repo.insert_or_update_author(%{ username: "foo" })
+    author1 = Repo.insert_or_update_author(%{username: "foo"})
 
     # Mark-as-resolved only gives us email
-    author2 = Repo.insert_or_update_author(%{ email: "foo@example.com" })
+    author2 = Repo.insert_or_update_author(%{email: "foo@example.com"})
 
     assert author1.id != author2.id
   end
--- web/models/author.ex
+++ web/models/author.ex
@@ -3,16 +3,17 @@ defmodule Review.Author do
   import Ecto.Changeset
 
   schema "authors" do
-    field :name, :string
-    field :email, :string
-    field :username, :string
-    field :created_at, Ecto.DateTime
-    field :updated_at, Ecto.DateTime
+    field(:name, :string)
+    field(:email, :string)
+    field(:username, :string)
+    field(:created_at, Ecto.DateTime)
+    field(:updated_at, Ecto.DateTime)
   end
 
   def changeset(author, params \\ %{}) do
     author
     |> cast(params, [:name, :email, :username])
+    |> unique_constraint(:email)
+    |> unique_constraint(:username)
   end
 end
-
